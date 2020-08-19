0260798251d43fce0b07b792dc633aa14f650576
Angel Posada Garcia <angelus9110@gmail.com>
2020-08-19 14:44:02 +0200

    Product and item microservices working with Feign and Eureka
    En este punto eliminamos las dependencias de ribbon pues ya esta incluido en eureka, pero si mantenemos Feign pues es necesario para identificar product desde item, ya eureka recibe el registro desde los clientes al iniciarse y los gestiona automaticamente junto al balanceo de cargas de los mismos

--- productservice/src/main/java/com/spingboot/microservices/products/productservice/ProductServiceApplication.java
+++ productservice/src/main/java/com/spingboot/microservices/products/productservice/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package com.spingboot.microservices.products.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
