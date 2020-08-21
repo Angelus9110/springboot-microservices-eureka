beb26bf54bc453e028bd1326f82f1dff8c46109f
Angel Posada Garcia <angelus9110@gmail.com>
2020-08-21 14:25:25 +0200

    Add CRUD operations in product service

--- productservice/src/main/java/com/spingboot/microservices/products/productservice/controllers/ProductController.java
+++ productservice/src/main/java/com/spingboot/microservices/products/productservice/controllers/ProductController.java
@@ -8,9 +8,9 @@ import com.spingboot.microservices.products.productservice.models.service.Produc
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class ProductController{
 
@@ -39,5 +39,12 @@ public class ProductController{
 		}*/
 		return productService.findById(id);
 	}
+	
+	@PostMapping("/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product createProduct(@RequestBody Product product){
+		return productService.save(product);
+	}
+
 
 }
