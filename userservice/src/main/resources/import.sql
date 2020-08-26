731e806666f8a47878405d1262419ad038878d9d
Angel Posada Garcia <angelus9110@gmail.com>
2020-08-26 10:43:38 +0200

    Create access level with oauth2 for user regarding each role
    Con este apartado podemos filtrar los accesos a los diferentes endpoints de nuestra aplicacion segun si el usuario esta o no autenticado e internamente segun el rol al que pertenezca

--- userservice/src/main/resources/import.sql
+++ userservice/src/main/resources/import.sql
@@ -6,6 +6,6 @@ INSERT INTO users (username, password, enabled, name, surname, email) VALUES('an
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 INSERT INTO roles(name) VALUES('ROLE_USER');
 
-INSERT INTO user_roles(user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles(user_id, role_id) VALUES (1, 2);
 INSERT INTO user_roles(user_id, role_id) VALUES (2, 2);
 INSERT INTO user_roles(user_id, role_id) VALUES (2, 1);
